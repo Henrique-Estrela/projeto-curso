3413c961e020cd5141b7d9724112e4640a39fcb8
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2022-04-19 20:30:39 -0300

    Update Mecanico.java

--- Projeto1/Projeto/src/Model/Mecanico.java
+++ Projeto1/Projeto/src/Model/Mecanico.java
@@ -77,5 +77,44 @@ public class Mecanico extends Funcionario{
        }
    }
     
-}
+    public static void verOrdenDeServico(){
+        File dir = new File("C:\\ArquivosProjeto");
+        //Cria um novo arquivo
+        File arq = new File(dir, "OrdenDeServico.txt");
+        
+        try {
+            FileReader fileReader = new FileReader(arq);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String linha = "";
 
+            //Lista que irá guardar o resultado, ou seja,
+            // cada linha do arquivo que corresponde a um User
+            List <String> result = new ArrayList();
+
+            while ((linha = bufferedReader.readLine()) != null) {
+                System.out.println(linha);
+                if (linha != null && !linha.isEmpty()) {
+                    result.add(linha);
+                }
+            }
+            fileReader.close();
+            bufferedReader.close();
+
+            for (String s : result) {
+                //Usamos o método split da classe String
+                // para separar as partes entre os ponto e vírgulas.
+                //Guardamos o resultado em um array
+                String[] user = s.split(";");
+                Double valor = Double.parseDouble(user[2]);
+                //Criamos um objeto User e setamos em seus atributos
+                //as posições correspondentes do array
+                Orcamento u = new Orcamento(user[0], user[1], valor, user[3]);
+
+                //exibe o conteúdo do objeto u
+                System.out.println(u.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
